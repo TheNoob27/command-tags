993a1d5697c071952a2d775e4db4437234fc8e97
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-04-29 21:02:37 +0100

    don't steal all the spaces

--- src/index.js
+++ src/index.js
@@ -70,8 +70,9 @@ module.exports = function Tagify(options = {}, ...tags) {
     
     return t
   }).join("|" + prefix) + ") ?", "g", "i"), t => {
-    if (!string.endsWith(t) && (t.endsWith(" ") || t.startsWith(" "))) t = t.trim() + " "
-    else t = t.trim()  
+    if (t.startsWith(" ") && t.endsWith(" ")) string = string.slice(0, string.indexOf(t)) + " " string.slice(string.indexOf(t), string.length)
+    else t = t.trim()
+    
     if (prefix.includes("|")) t = t.replace(new RegExp(prefix, "i"), "")
     else t = t.slice(prefix.length)
     
