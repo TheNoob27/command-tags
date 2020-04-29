62c003c78ca16dcc19d3047db1cd1e7a0e402461
TheNoob27 <54063052+TheNoob27@users.noreply.github.com>
2020-04-29 19:14:29 +0100

    tagified

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
+/**
+ * @typedef {Object} Tag
+ * @property {string} tag The tag to recognise.
+ * @property {Number|String|string} value The value type the tag should have. e.g "Number"
+ */
+
+/**
+ * @typedef {Object} Options
+ * @property {string} string The string to parse command tags from.
+ * @property {string|RegExp} [prefix="--"] The prefix that would recognise a word as a tag. This can be a String or Regular Expression. e.g "--big", "--" being the prefix.
+ */
+
+/**
+ * @typedef {Object} ParsedTags
+ * @property {string} string The original string.
+ * @property {string} newString The new string with all valid tags removed.
+ * @property {array} matches All valid tags the string contained.
+ * @property {object} data All valid tags thats had values and their values that the string contained.
+ */
+
+/**
+ * Get custom command tags out of a string.
+ * @param {Options|string} options The options to pass in, or the string to parse tags from.
+ * @param {...(string|Tag)} tags Tags to recognise. You can pass in "\w+" to recognise anything, or a tag object to make the tag have a value (e.g "--size 10"). Tags with values will be put in the data object.
+ * @returns {ParsedTags}
+ * @example
+ * ```
+ * Tagify({
+ *   string: "Write text --bold --italic"
+ *   prefix: "--"
+ * }, "bold", "italic", "strikethrough", "underline")
+ * // -> {
+ * //   string: "Write text --bold --italic",
+ * //   newString: "Write text"
+ * //   matches: ["bold", "italic"],
+ * //   data: {}
+ * // }
+ * ```
+ */
 module.exports = function Tagify(options = {}, ...tags) {
   let matches = []
   let data = {}
@@ -9,7 +48,9 @@ module.exports = function Tagify(options = {}, ...tags) {
     prefix = "(-|--)"
   } else if (options && typeof options === "object") {
     { string, prefix } = options
-  } else [string, prefix] = ["", "--"]
+  }
+  
+  if (!string || !prefix) [string, prefix] = [string || "", prefix || "--"]
   
   let newString = tags[0] ? string.replace(new RegExp(" ?(" + prefix + tags.map(t => {
     if (typeof t === "string" && t.includes(" ")) t = {tag: t.split(/ +/)[0], value: t.split(/ +/)[1]}
